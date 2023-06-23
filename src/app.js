2d101d4fee38e74ac2402925448fe97a68a23d83
nathbalica <nathbalica@gmail.com>
2023-06-23 11:16:02 -0300

    formatting

--- src/app.js
+++ src/app.js
@@ -7,26 +7,10 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const users = [
-    {
-        username: 'bobesponja', 
-        avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-    }
-]
+const users = []
+const tweets = []
 
-const tweets = [
-    
-    {
-        username: "bobesponja",
-        tweet: "Eu amo hambúrguer de siri!"
-    }
-    
-]
-
-app.get("/sign-up", (req, res) => {
-    res.send(users)
-})
-
+// Endpoint: Cadastrar um novo usuário
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
 
@@ -42,21 +26,20 @@ app.post("/sign-up", (req, res) => {
     const userSignUp = {
         username: username,
         avatar: avatar
-    }; 
+    };
 
     users.push(userSignUp)
     res.status(201).send('OK');
 });
 
-
+// Endpoint: Criar um novo tweet
 app.post("/tweets", (req, res) => {
 
     const { tweet } = req.body;
     const user = req.headers.user;
-
-    console.log(user)
     const tweetValidationResult = validateTweet(user, tweet);
-    if(tweetValidationResult){
+
+    if (tweetValidationResult) {
         res.status(400).send(tweetValidationResult);
         return;
     }
@@ -65,20 +48,36 @@ app.post("/tweets", (req, res) => {
         username: user,
         tweet: tweet
     };
-    console.log(tweetsUser)
+
     const userExists = users.find(u => u.username === user);
 
-    if(userExists){
+    if (userExists) {
         tweets.push(tweetsUser)
         res.status(201).send('OK');
-    }else{
+    } else {
         res.status(401).send('UNAUTHORIZED')
     }
 })
 
-app.get("/tweets", (req, res) => {
-    const page = parseInt(req.query.page); // Obter o valor da página da query string como um número inteiro
+// Endpoint: Obter tweets de um usuário específico
+app.get("/tweets/:username", (req, res) => {
+    const { username } = req.params;
 
+    const userTweets = tweets.find(tweet => tweet.username === username);
+    const user = users.find(user => user.username === username)
+
+    const userTweetsWithAvatar = userTweets.map(tweet => ({
+        username: tweet.username,
+        avatar: user ? user.avatar : '',
+        tweet: tweet.tweet
+    }));
+
+    res.send(userTweetsWithAvatar);
+})
+
+// Endpoint: Obter tweets por página
+app.get("/tweets", (req, res) => {
+    const page = parseInt(req.query.page);
     if (!page || page < 1 || isNaN(page)) {
         res.status(400).send('Informe uma página válida!');
         return;
@@ -100,22 +99,6 @@ app.get("/tweets", (req, res) => {
     });
 
     res.send(tweetsForPage);
-})
-
-
-app.get("/tweets/:username", (req, res) => {
-    const { username } = req.params;
-    
-    const userTweets = tweets.find(tweet => tweet.username === username);
-    const user = users.find(user => user.username === username)
-
-    const userTweetsWithAvatar = userTweets.map(tweet => ({
-        username: tweet.username,
-        avatar: user ? user.avatar : '',
-        tweet: tweet.tweet
-    }));
-    
-    res.send(userTweetsWithAvatar);
 })
 
 app.listen(5000, () => console.log("Running server on port 5000"))
